a090b5496ec19bdd266bd16ddc2328d7bdfccd4f
Pougliche <pugliese.viktor@gmail.com>
2019-10-08 15:54:45 +0200

    Ajout des fichiers multiply spec

---
+++ spec/00_multiplyv_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/00_multiplyv'
+
+describe "the is_multiple_of_3_or_5? method" do
+    it "should return TRUE when an integer is a multiple of 3 or 5" do
+      expect(is_multiple_of_3_or_5?(3)).to eq(true)
+      expect(is_multiple_of_3_or_5?(5)).to eq(true)
+      expect(is_multiple_of_3_or_5?(51)).to eq(true)
+      expect(is_multiple_of_3_or_5?(45)).to eq(true)
+    end
+  
+    it "should return FALSE when an integer is NOT a multiple of 3 or 5" do
+      expect(is_multiple_of_3_or_5?(14)).to eq(false)
+      expect(is_multiple_of_3_or_5?(64)).to eq(false)
+      expect(is_multiple_of_3_or_5?(77)).to eq(false)
+      expect(is_multiple_of_3_or_5?(4)).to eq(false)
+    
+    end
+  end
